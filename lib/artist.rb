dfabc81d561e2dad4a3106e68dfc2920b705f388
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-18 02:03:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,8 +17,12 @@ class Artist
     @all
   end
   
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
   def add_song(song)
-    @songs << song 
+    song.artist = self
   end
   
   def self.find(name)
