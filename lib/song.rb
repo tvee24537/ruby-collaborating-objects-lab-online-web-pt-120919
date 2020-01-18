75c82c4aa86c59fb2a97842594f69e9d33d6bd21
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-18 02:10:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,25 +1,21 @@
-class Song 
-  
-  attr_accessor :name, :artist 
-  
+class Song
+  attr_accessor :name, :artist
+
   def initialize(name)
     @name = name
-    
-  end
-
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
   end
 
   def self.new_by_filename(file)
-    song_name = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_name[1])
-    song.artist_name = song_name[0]
+    artist_name = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist_name = artist_name
     song
   end
-  
-  def self.all
-    @@all
-  end  
-end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+  end
+end 
