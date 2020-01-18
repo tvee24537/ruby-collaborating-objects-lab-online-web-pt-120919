b06bc57a41842e48a3075851fa00fcaafc417d80
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-18 02:22:52 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,8 +10,6 @@ class MP3Importer
   end
 
   def import
-    self.files.each do |filename|
-      Song.new_by_filename(filename)
-    end
+    files.each {|song| Song.new_by_filename(song)}
   end
 end
